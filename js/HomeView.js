65732786dba4719e65d6252e8cf4ff1dacbe13af
kchiou <kenny.chiou@gmail.com>
2014-01-22 11:24:20 -0500

    Styles and Touch Scrolling

--- js/HomeView.js
+++ js/HomeView.js
@@ -10,10 +10,17 @@ var HomeView = function(store) {
 		this.el.html(HomeView.template());
 		return this;
 	};
-	
+
 	this.findByName = function() {
 		store.findByName($('.search-key').val(), function(employees) {
 			$('.employee-list').html(HomeView.liTemplate(employees));
+			if (self.iscroll) {
+				console.log('Refresh iScroll');
+				self.iscroll.refresh();
+			} else {
+				console.log('New iScroll');
+				self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+			}
 		});
 	};
 
